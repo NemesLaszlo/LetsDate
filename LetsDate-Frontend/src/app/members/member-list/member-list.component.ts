fad47a762de26d8188685ebc81e6ef175a3f108e
NemesLaszlo <wow.laszlo@gmail.com>
2021-04-22 11:23:31 +0200

    frontend components (list, messages, memeber-list, member-detail) with routing

---
+++ LetsDate-Frontend/src/app/members/member-list/member-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-member-list',
+  templateUrl: './member-list.component.html',
+  styles: [
+  ]
+})
+export class MemberListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
