71389fd3992d4f4fc64226c763c329c564c056cc
NemesLaszlo <wow.laszlo@gmail.com>
2021-05-07 16:51:15 +0200

    frontend members-list filters age and gender

--- LetsDate-Frontend/src/app/members/member-list/member-list.component.ts
+++ LetsDate-Frontend/src/app/members/member-list/member-list.component.ts
@@ -17,6 +17,7 @@ export class MemberListComponent implements OnInit {
   pagination: Pagination;
   userParams: UserParams;
   user: User;
+  genderList = [{ value: 'male', display: 'Males' }, { value: 'female', display: 'Females' }];
 
   constructor(private memberService: MembersService, private accountService: AccountService) {
     this.accountService.currentUser$.pipe(take(1)).subscribe(user => {
@@ -38,6 +39,11 @@ export class MemberListComponent implements OnInit {
 
   pageChanged(event: any) {
     this.userParams.pageNumber = event.page;
+    this.loadMembers();
+  }
+
+  resetFilters() {
+    this.userParams = new UserParams(this.user);
     this.loadMembers();
   }
 
