20e46b071ed8cf7e26c249dd66a63adf571b60b9
NemesLaszlo <wow.laszlo@gmail.com>
2021-05-22 23:49:04 +0200

    frontend Admin functionality for role management with modals

--- LetsDate-Frontend/src/app/app.module.ts
+++ LetsDate-Frontend/src/app/app.module.ts
@@ -27,6 +27,9 @@ import { DateInputComponent } from './_forms/date-input/date-input.component';
 import { MemberMessagesComponent } from './members/member-messages/member-messages.component';
 import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
 import { HasRoleDirective } from './_directives/has-role.directive';
+import { UserManagementComponent } from './admin/user-management/user-management.component';
+import { PhotoManagementComponent } from './admin/photo-management/photo-management.component';
+import { RolesModalComponent } from './modals/roles-modal/roles-modal.component';
 
 @NgModule({
   declarations: [
@@ -48,7 +51,10 @@ import { HasRoleDirective } from './_directives/has-role.directive';
     DateInputComponent,
     MemberMessagesComponent,
     AdminPanelComponent,
-    HasRoleDirective
+    HasRoleDirective,
+    UserManagementComponent,
+    PhotoManagementComponent,
+    RolesModalComponent
   ],
   imports: [
     BrowserModule,
@@ -64,6 +70,7 @@ import { HasRoleDirective } from './_directives/has-role.directive';
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true }
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [RolesModalComponent], // Ivy disabled (in tsconfig.json), so need that
 })
 export class AppModule { }
